d31710ed1a5ce50039abf26a9476817ab81fd0be
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-12-18 16:23:28 +0100

    init

---
+++ preconfig_odoo_server.sh
@@ -0,0 +1,21 @@
+SERVER_HOST_NAME="s06.beopen.cloud"
+SERVER_TIMEZONE="Europe/Madrid"
+
+# change server hostname
+sudo hostnamectl set-hostname ${SERVER_HOST}
+# change server timezone
+timedatectl set-timezone ${SERVER_TIMEZONE}
+
+# change auto update config
+cat <<EOF > ~/server-config-upgrades
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "0";
+EOF
+sudo mv ~/server-config-upgrades /etc/apt/apt.conf.d/20auto-upgrades
+sudo rm ~/server-config-upgrades
+
+# update and upgrade server SO
+sudo apt-get update
+sudo apt-get upgrade
+
+echo -e "* Preconfig Odoo Server Done!"
